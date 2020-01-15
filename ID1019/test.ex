229bbf35d5bdbe7ba3b2aa8c190aed5ffff6fdf0
plut092 <johan.m.engstrom92@gmail.com>
2020-01-15 22:03:41 +0100

    first commit

---
+++ ID1019/test.ex
@@ -0,0 +1,28 @@
+defmodule Test do
+
+  # Compute the double of a number.
+  def double(n) do
+    2*n
+  end
+
+  # Fahrenheit to Celsius
+  def f2c(n) do
+    (n-32)/1.8
+  end
+
+  # Rectangle area
+  def rect_area(b,h) do
+    b*h
+  end
+
+  # Square area
+  def sqr_area(n) do
+    rect_area(n,n)
+  end
+
+  # Circle area
+  def circle_area(r) do
+    :math.pi()*r*r
+  end
+
+end
